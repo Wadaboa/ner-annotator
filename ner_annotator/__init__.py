dcbe05439b4d15a29b8acbe0ed344bc71a64ae1d
Alessio Falai <falai.alessio@gmail.com>
2020-09-26 15:21:03 +0200

    minor fixes

--- ner_annotator/__init__.py
+++ ner_annotator/__init__.py
@@ -8,4 +8,4 @@ from .annotator import NERAnnotator
 from .model import load_model
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+_PATH=$(dirname "$(realpath $0)")
+
+cd $_PATH
 pip install -r requirements-deploy.txt
 
-_PATH=$(dirname "$(realpath $0)")
 cd $_PATH/..
-
 pip install .
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
